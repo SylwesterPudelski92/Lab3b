5ea10d5cd57a69f1e61fdd9fed303b4dc833d5b9
SylwesterPudelski92 <sylwesterpudelski1992@gmail.com>
2022-05-14 16:23:17 +0200

    Połączenie z bazą danych w Java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.example.lab7 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires mysql.connector.java;
 
 
     opens pl.lublin.wsei.java.cwiczenia to javafx.fxml;
--- src/main/java/pl/lublin/wsei/java/cwiczenia/test/TestConnection.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/test/TestConnection.java
@@ -1,4 +1,27 @@
 package pl.lublin.wsei.java.cwiczenia.test;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.*;
+import java.util.Properties;
+
+import com.mysql.jdbc.Driver;
+
 public class TestConnection {
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        String serverName = "localHost";
+        String database = "mycb";
+        Number portNumber = 3306;
+        Connection conn = null;
+        Properties connectionProps = new Properties();
+        connectionProps.put("user", "root");
+        connectionProps.put("password", "Wojsko11Wojsko");
+        connectionProps.put("serverTimezone", "Europe/Warsaw");
+
+        conn = DriverManager.getConnection(
+                "jdbc:mysql://" + serverName + ":" + portNumber + "/"+ database, connectionProps);
+        System.out.println("Connected to database");
+
+    }
 }
